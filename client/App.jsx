4dbffd64a3af13667f5de1921b35b79448ad5f0c
gvozdikov <andrey.gvozdikov@fastdev.se>
2015-08-16 21:46:04 +0200

    today flights logic

--- client/App.jsx
+++ client/App.jsx
@@ -3,14 +3,22 @@ App = React.createClass({
   // This mixin makes the getMeteorData method work
   mixins: [ReactMeteorData],
 
-  // Loads items from the Tasks collection and puts them on this.data.tasks
+  // Loads items from the Flights collection and puts them on this.data.tasks
   getMeteorData() {
+    var now = new Date();
     return {
-      flights: Flights.find({}).fetch()
+      flights: Flights.find({}).fetch(),
+      todayFlights: Flights.find({
+        departure_date_time: {
+          $gt: new Date(now.getFullYear(), now.getMonth(), now.getDate()),
+          $lt: new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1)
+        }
+      }).fetch()
     }
   },
 
   renderFlights() {
+    console.log(this.data.todayFlights);
     return this.data.flights.map((flight) => {
       return <Flight key={flight._id} flight={flight} />;
     });
