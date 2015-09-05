06e338200f5f7c08055edd5675d48278dd95ddad
gvozdikov <andrey.gvozdikov@fastdev.se>
2015-09-05 18:37:36 +0200

    delete flight

--- client/Flight.jsx
+++ client/Flight.jsx
@@ -2,10 +2,20 @@ Flight = React.createClass({
   propTypes: {
     flight: React.PropTypes.object.isRequired
   },
+
+  deleteThisFlight() {
+    Flights.remove(this.props.flight._id);
+  },
+
   render() {
   var date_time = moment(this.props.flight.departure_date_time).format('YYYY-MM-DD HH:mm');
     return (
-      <li>{this.props.flight.flight_number} {date_time}</li>
+      <li>
+        <button className="delete" onClick={this.deleteThisFlight}>
+          &times;
+        </button>
+        <span>{this.props.flight.flight_number} {date_time}</span>
+      </li>
     );
   }
 });
