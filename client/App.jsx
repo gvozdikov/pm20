200c07f6f5cad2ac28b21dece4f989d2343e6de8
gvozdikov <andrey.gvozdikov@fastdev.se>
2015-09-05 18:18:56 +0200

    add flight for today form

--- client/App.jsx
+++ client/App.jsx
@@ -29,18 +29,45 @@ App = React.createClass({
     }
   },
 
-  filterChange(event) {
+  handleAddFlight(event) {
+    event.preventDefault();
+
+    // Find the text field via the React ref
+    var text = React.findDOMNode(this.refs.textInput).value.trim();
+
+    Flights.insert({
+      flight_number: text,
+      departure_date_time: new Date()
+    });
+
+    // Clear form
+    React.findDOMNode(this.refs.textInput).value = "";
+  },
+
+  handleFilterChange(event) {
     this.setState({value: event.target.value});
-    // event.preventDefault();
   },
 
   render() {
     return (
       <div className="container">
-        <select onChange={this.filterChange} >
-          <option value="all">All flights</option>
-          <option value="today">Today flights</option>
-        </select>
+        <header>
+          <h1>Flight List</h1>
+          <form className="new-flight" onSubmit={this.handleAddFlight} >
+            <lable>
+              Add flight:
+              <input
+                type="text"
+                ref="textInput"
+                placeholder="Flight number" />
+            </lable>
+          </form>
+          <select onChange={this.handleFilterChange} >
+            <option value="all">All flights</option>
+            <option value="today">Today flights</option>
+          </select>
+        </header>
+
         <ul>
           {this.renderFlights()}
         </ul>
